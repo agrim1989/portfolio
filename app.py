a449afb3b9e85ed129a86c17f7b4542f15bd73e8
agrim1989 <agrim89@gmail.com>
2024-04-09 06:06:35 +0000

    udpate Make file

--- app.py
+++ app.py
@@ -2,28 +2,29 @@
 # An object of Flask class is our WSGI application.
 from flask import Flask, render_template
 
-# Flask constructor takes the name of 
+# Flask constructor takes the name of
 # current module (__name__) as argument.
 app = Flask(__name__)
 
-# The route() function of the Flask class is a decorator, 
-# which tells the application which URL should call 
+
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
 # the associated function.
-@app.route('/')
+@app.route("/")
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-   return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/contacts')
+@app.route("/contacts")
 # ‘/’ URL is bound with hello_world() function.
 def contacts():
-   return render_template('contact.html')
+    return render_template("contact.html")
 
 
 # main driver function
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-	# run() method of Flask class runs the application 
-	# on the local development server.
-	app.run()
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run()
